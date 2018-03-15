076fb16484dafd296a2cdfba1210b38b9d8073e9
Nick <coding.saints2015@gmail.com>
2018-03-15 18:22:25 +0000

    dev branch

--- src/index.js
+++ src/index.js
@@ -1,51 +1,11 @@
-
 import React from 'react'
 import {render} from 'react-dom'
 
-const ThemeContext = React.createContext('light')
-class ThemeProvider extends React.Component {
-  state = {theme: 'light'}
-  toggleTheme = () => {
-    this.setState(({theme}) => ({
-      theme: theme === 'light' ? 'dark' : 'light',
-    }))
-  }
+export default class App extends React.Component {
   render() {
-    return (
-      <ThemeContext.Provider value={this.state.theme}>
-        <button onClick={this.toggleTheme}>toggle theme</button>
-        {this.props.children}
-      </ThemeContext.Provider>
-    )
+    return <div>hi paul</div>
   }
 }
-// only doing this to shield end-users from the
-// implementation detail of "context"
-const ThemeConsumer = ThemeContext.Consumer
-
-const styles = {
-  dark: {
-    backgroundColor: 'black',
-    color: 'white',
-  },
-  light: {
-    backgroundColor: 'white',
-    color: 'black',
-  },
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <ThemeProvider>
-        <ThemeConsumer>
-          {theme => <div style={styles[theme]}>{theme}</div>}
-        </ThemeConsumer>
-      </ThemeProvider>
-    )
-  }
-}
-
 
 render(
     <App />,
